1be326130025bd542728d4fa92545525f692ed41
Greg Weber <greg@gregweber.info>
2014-01-02 19:02:38 -0800

    parse media queries

--- runtests.hs
+++ runtests.hs
@@ -9,47 +9,67 @@ import Data.Text.Lazy (toStrict)
 import Data.Text (Text)
 import Test.QuickCheck
 import Control.Arrow ((***))
+import Control.Monad (liftM)
 
 main :: IO ()
 main = hspec $ do
-    describe "single attribute parser" $ do
-        it "trimming whitespace" $
-            parseAttr "   foo   : bar   " `shouldBe` Right ("foo", "bar")
-    describe "multiple attribute parser" $ do
-        it "no final semicolon" $
-            parseAttrs " foo: bar ;  baz : bin  "
-                `shouldBe` Right [("foo", "bar"), ("baz", "bin")]
-        it "final semicolon" $
-            parseAttrs " foo: bar ;  baz : bin  ;"
-                `shouldBe` Right [("foo", "bar"), ("baz", "bin")]
-        it "ignores comments" $
-            parseAttrs " foo: bar ; /* ignored */ baz : bin  ;"
-                `shouldBe` Right [("foo", "bar"), ("baz", "bin")]
-    describe "block parser" $ do
-        it "multiple blocks" $
-            parseBlocks (T.concat
-            [ "foo{fooK1:fooV1;/*ignored*/fooK2:fooV2               }\n\n"
-            , "/*ignored*/"
-            , "bar{barK1:barV1;/*ignored*/barK2:barV2               ;}\n\n/*ignored*/"
-            ]) `shouldBe` Right [
-              ("foo", [("fooK1", "fooV1"), ("fooK2", "fooV2")])
-            , ("bar", [("barK1", "barV1"), ("barK2", "barV2")])
+  describe "single attribute parser" $ do
+    it "trimming whitespace" $
+      parseAttr "   foo   : bar   " `shouldBe` Right ("foo", "bar")
+
+  describe "multiple attribute parser" $ do
+    it "no final semicolon" $
+      parseAttrs " foo: bar ;  baz : bin  "
+          `shouldBe` Right [("foo", "bar"), ("baz", "bin")]
+
+    it "final semicolon" $
+      parseAttrs " foo: bar ;  baz : bin  ;"
+          `shouldBe` Right [("foo", "bar"), ("baz", "bin")]
+
+    it "ignores comments" $
+      parseAttrs " foo: bar ; /* ignored */ baz : bin  ;"
+          `shouldBe` Right [("foo", "bar"), ("baz", "bin")]
+
+  describe "block parser" $ do
+    it "multiple blocks" $
+      parseBlocks (T.concat
+      [ "foo{fooK1:fooV1;/*ignored*/fooK2:fooV2               }\n\n"
+      , "/*ignored*/"
+      , "bar{barK1:barV1;/*ignored*/barK2:barV2               ;}\n\n/*ignored*/"
+      ]) `shouldBe` Right [
+        ("foo", [("fooK1", "fooV1"), ("fooK2", "fooV2")])
+      , ("bar", [("barK1", "barV1"), ("barK2", "barV2")])
+      ]
+
+    it "media queries" $ do
+      parseBlocks "@media print {* {text-shadow: none !important;} }"
+        `shouldBe` Right []
+      parseNestedBlocks "@media print {* {text-shadow: none !important; color: #000 !important; } a, a:visited { text-decoration: underline; }}"
+        `shouldBe` Right [NestedBlock "@media print"
+            [ LeafBlock ("*", [("text-shadow", "none !important"), ("color", "#000 !important")])
+            , LeafBlock ("a, a:visited", [("text-decoration", "underline")])
             ]
+          ]
 
-    describe "render" $ do
-        it "works" $
-            renderBlocks [
-                  ("foo", [("bar", "baz"), ("bin", "bang")])
-                , ("foo2", [("x", "y")])
-                ]
-                `shouldBe` "foo{bar:baz;bin:bang}foo2{x:y}"
+  describe "render" $ -- do
+    it "works" $
+      renderBlocks [
+            ("foo", [("bar", "baz"), ("bin", "bang")])
+          , ("foo2", [("x", "y")])
+          ]
+          `shouldBe` "foo{bar:baz;bin:bang}foo2{x:y}"
 
-    describe "parse/render" $ do
-        prop "is idempotent" $ \bs ->
-            parseBlocks (toStrict $ toLazyText $ renderBlocks $ unBlocks bs) == Right (unBlocks bs)
+  describe "parse/render" $ do
+    prop "idempotent blocks" $ \bs ->
+      parseBlocks (toStrict $ toLazyText $ renderBlocks $ unBlocks bs) == Right (unBlocks bs)
+    prop "idempotent nested blocks" $ \bs ->
+      parseNestedBlocks (toStrict $ toLazyText $ renderNestedBlocks bs) == Right bs
 
 newtype Blocks = Blocks { unBlocks :: [(Text, [(Text, Text)])] }
     deriving (Show, Eq)
+
+instance Arbitrary NestedBlock where
+    arbitrary = (LeafBlock . unBlock) `liftM` arbitrary
 
 instance Arbitrary Blocks where
     arbitrary = fmap (Blocks . map unBlock) arbitrary
